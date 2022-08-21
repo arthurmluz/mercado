7611e680053dd74933d5c816efac67619aef476e
Arthur Musskopf da Luz <arthurmluz123@gmail.com>
2022-08-21 00:11:57 -0300

    missed register routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
+
+from app.adaptadores.controllers import register_routes
 
 import secrets
 
@@ -8,11 +11,16 @@ db = SQLAlchemy()
 
 conn = "postgresql://{0}:{1}@{2}:5432/{3}".format(secrets.dbpass, secrets.dbuser, secrets.dbhost, secrets.dbname)
 
+root_blueprint = Blueprint('root_blueprint', __name__)
+root_api = Api(root_blueprint, title='API mercado', version='1.0', description='Mercado')
+
 
 def create_app():
     app = Flask(__name__)
 
     init_extensions(app)
+
+    init_blueprints(app)
 
     return app
 
@@ -22,4 +30,9 @@ def init_extensions(app):
     app.config['SECRET_KEY'] = 'SuperSecretKey'
     db.init_app(app)
 
+
+def init_blueprints(app):
+    app.register_blueprint(root_blueprint)
+
+    register_routes(root_api)
 
